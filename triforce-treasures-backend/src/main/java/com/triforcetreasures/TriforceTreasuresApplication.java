b52952b6247b76d1f60527846e8fc6ad6af78601
Mateus Silva Freitas <mateus.freitas@paytrack.com.br>
2023-12-12 10:42:41 -0300

    Project structure

---
+++ triforce-treasures-backend/src/main/java/com/triforcetreasures/TriforceTreasuresApplication.java
@@ -0,0 +1,13 @@
+package com.triforcetreasures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriforceTreasuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriforceTreasuresApplication.class, args);
+	}
+
+}
---
+++ triforce-treasures-backend/src/test/java/com/triforcetreasures/TriforceTreasuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triforcetreasures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriforceTreasuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
